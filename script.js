4bdbef8db387b5eaa7c93991b0f2248946926b18
Jeff <jeffreyjohn102@gmail.com>
2024-09-19 21:22:50 -0400

    Add functions for creation and placement of note icons

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ noteList.forEach(note => {
     note.addEventListener('click', function() {
         console.log(this.dataset.note);
         noteDisplay.textContent = this.dataset.note;
+        createNoteIcon(this);
     });
 });
+
+function createNoteIcon(noteElement) {
+    const noteIcon = document.createElement('div');
+    noteIcon.classList.add('note-icon');
+    noteIcon.textContent = noteElement.dataset.note;
+
+    appendNewElement(noteElement, noteIcon);
+}
+
+function appendNewElement(parent, child) {
+    parent.append(child);
+}
